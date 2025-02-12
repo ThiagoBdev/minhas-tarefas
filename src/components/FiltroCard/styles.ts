92579532073174313f8c5aca5b5cb9335be7831d
Thiago Batista <thiagobatista15390@gmail.com>
2025-02-12 19:50:40 -0300

    construindo o aside

---
+++ src/components/FiltroCard/styles.ts
@@ -0,0 +1,20 @@
+import styled from 'styled-components'
+import { Props } from '.'
+
+export const Card = styled.div<Props>`
+  padding: 8px;
+  border: 1px solid ${(props) => (props.ativo ? '#1E90FF' : '#A1A1A1')};
+  color: ${(props) => (props.ativo ? '#1e90FF' : '#5e5e5e')};
+  background-color: ${(props) => (props.ativo ? '#FFF' : '#fcfcfc')};
+  border-radius: 8px;
+`
+
+export const Contador = styled.span`
+  font-weight: bold;
+  font-size: 24px;
+  display: block;
+`
+
+export const Label = styled.span`
+  font-size: 14px;
+`
---
+++ src/containers/ListaDeTarefas/index.tsx
@@ -0,0 +1,7 @@
+const ListaDeTarefas = () => (
+  <main>
+    <p>2 tarefas marcadas como: &quot;categoria&lquo; e &quot;termo&lquo;</p>
+  </main>
+)
+
+export default ListaDeTarefas
