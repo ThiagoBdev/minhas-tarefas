ab741e3a0d852797544d907a7832a2a98d395301
Thiago Batista <thiagobatista15390@gmail.com>
2025-02-13 08:15:25 -0300

    corrigindo o Aside

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,9 @@ function App() {
   return (
     <>
       <EstiloGlobal />
-      <Container />
-      <BarraLateral />
+      <Container>
+        <BarraLateral />
+      </Container>
       <ListaDeTarefas />
     </>
   )
--- src/components/FiltroCard/styles.ts
+++ src/components/FiltroCard/styles.ts
@@ -1,7 +1,9 @@
 import styled from 'styled-components'
 import { Props } from '.'
 
-export const Card = styled.div<Props>`
+type PropsSemLegendaEContador = Omit<Props, 'contador' | 'legenda'>
+
+export const Card = styled.div<PropsSemLegendaEContador>`
   padding: 8px;
   border: 1px solid ${(props) => (props.ativo ? '#1E90FF' : '#A1A1A1')};
   color: ${(props) => (props.ativo ? '#1e90FF' : '#5e5e5e')};
