843992dd40993c3b604932fedc30ede28b61a4aa
Thiago Batista <thiagobatista15390@gmail.com>
2025-02-14 10:10:57 -0300

    criando as constantes contador e ativo

--- src/components/FiltroCard/styles.ts
+++ src/components/FiltroCard/styles.ts
@@ -1,16 +1,16 @@
 import styled from 'styled-components'
-import { Props } from '.'
 
-type PropsSemLegendaEContador = Omit<Props, 'contador' | 'legenda'>
+type Props = {
+  ativo: boolean
+}
 
-export const Card = styled.div.attrs<PropsSemLegendaEContador>(() => ({
-  ativo: undefined
-}))<PropsSemLegendaEContador>`
+export const Card = styled.div.attrs<Props>(() => ({}))<Props>`
   padding: 8px;
   border: 1px solid ${({ ativo }) => (ativo ? '#1E90FF' : '#A1A1A1')};
   color: ${({ ativo }) => (ativo ? '#1E90FF' : '#5E5E5E')};
   background-color: ${({ ativo }) => (ativo ? '#FFF' : '#FCFCFC')};
   border-radius: 8px;
+  cursor: pointer;
 `
 
 export const Contador = styled.span`
--- src/containers/BarraLateral/index.tsx
+++ src/containers/BarraLateral/index.tsx
@@ -3,6 +3,7 @@ import FiltroCard from '../../components/FiltroCard'
 import { RootReducer } from '../../store'
 import { alteraTermo } from '../../store/reducers/filtro'
 import * as S from './styles'
+import * as enums from '../../utils/enums/tarefa'
 
 const BarraLateral = () => {
   const dispatch = useDispatch()
@@ -18,12 +19,32 @@ const BarraLateral = () => {
           onChange={(evento) => dispatch(alteraTermo(evento.target.value))}
         />
         <S.Filtros>
-          <FiltroCard legenda="pendentes" contador={1} />
-          <FiltroCard legenda="concluido" contador={2} />
-          <FiltroCard legenda="urgentes" contador={3} />
-          <FiltroCard legenda="importantes" contador={4} />
-          <FiltroCard legenda="normal" contador={5} />
-          <FiltroCard ativo legenda="todas" contador={10} />
+          <FiltroCard
+            valor={enums.Status.PENDENTE}
+            criterio="status"
+            legenda="pendentes"
+          />
+          <FiltroCard
+            valor={enums.Status.CONCLUIDA}
+            criterio="status"
+            legenda="concluido"
+          />
+          <FiltroCard
+            valor={enums.Prioridade.URGENTE}
+            criterio="prioridade"
+            legenda="urgentes"
+          />
+          <FiltroCard
+            valor={enums.Prioridade.IMPORTANTE}
+            criterio="prioridade"
+            legenda="importantes"
+          />
+          <FiltroCard
+            valor={enums.Prioridade.NORMAL}
+            criterio="prioridade"
+            legenda="normal"
+          />
+          <FiltroCard criterio="todas" legenda="todas" />
         </S.Filtros>
       </div>
     </S.Aside>
