0bed8c46baf228a02fd76c36fcc9487951071ea0
Thiago Batista <thiagobatista15390@gmail.com>
2025-02-18 08:51:30 -0300

    Criando uma nova pagina

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,4 @@
+import BotaoAdicionar from '../../components/botaoAdicionar'
 import BarraLateral from '../../containers/BarraLateral'
 import ListaDeTarefas from '../../containers/ListaDeTarefas'
 
@@ -5,6 +6,7 @@ const Home = () => (
   <>
     <BarraLateral />
     <ListaDeTarefas />
+    <BotaoAdicionar />
   </>
 )
 export default Home
